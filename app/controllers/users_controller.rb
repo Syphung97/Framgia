86a0e9b39bb659f230ef7a8401529a368a327377
Sy Phung <syphung97@gmail.com>
2018-05-04 00:53:13 +0700

    chap7

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,33 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+    @genders = User.genders
+  end
+
+  def create
+    @genders = User.genders
+    @user = User.new user_params
+    if user.save
+      flash[:danger] = t "users.new.success"
+      redirect_to user
+    else
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find_by id: params[:id]
+
+    return if user
+    flash[:danger] = t "users.show.error"
+    redirect_to root_path
+  end
+  private
+
+  attr_reader :user
+
+  def user_params
+    params.require(:user).permit :name, :email, :password,
+      :password_confirmation, :gender
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,10 @@
 class User < ApplicationRecord
-  enum genders: [:male, :female]
-  before_validation :gender_valid
-
+  enum genders: [:female, :male]
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, format: {with: VALID_EMAIL_REGEX}, presence: true,
-    length: {maximum: maxemail}, uniqueness: {case_sensitive: false}
-  validates :password, presence: true, length: {minimum: minpassword}
-  validates :name, presence: true, length: {maximum: maxname}
+    length: {maximum: Settings.maxemail}, uniqueness: {case_sensitive: false}
+  validates :password, presence: true, length: {minimum: Settings.minpassword}
+  validates :name, presence: true, length: {maximum: Settings.maxname}
   validates :gender, inclusion: {in: genders.keys}
 
   before_save :email_down
